297934dc3c135ce93abd20f1b8f8f1176596d8df
Madhubala-06 <116252113+Madhubala-06@users.noreply.github.com>
2023-06-23 10:31:31 +0530

    Add new src and pom files in this repository

---
+++ src/main/java/com/fssa/mb/sampleprogram/day17/solved/HeloWorldProg.java
@@ -0,0 +1,11 @@
+package com.fssa.mb.sampleprogram.day17.solved;
+
+public class HeloWorldProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Helo World  ");
+
+	}
+
+}
